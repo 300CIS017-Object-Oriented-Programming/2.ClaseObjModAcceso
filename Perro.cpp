190948507bb68de88097355faeb3dbdc4155445b
Luisa Rincon <lufe089@gmail.com>
2024-07-22 10:27:28 -0500

    Update Perro.cpp

--- Perro.cpp
+++ Perro.cpp
@@ -9,10 +9,10 @@ void Perro::ladrar() {
     std::cout << " Esta ladrando " << nombre << " tengo " << edad << " anios " << std::endl;
 }
 
-void Perro::ladrar() {
+void Perro::saludar() {
     int veces = 5; // veces es una variable local
     for (int i = 0; i < veces; i++) {
        // Esta linea combina variables locales con atributos de instancia
-        std::cout << nombre << " ladra." << std::endl;
+        std::cout << nombre << " saluda." << std::endl;
     }
 }
